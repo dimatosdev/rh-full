ebc5b9f3736fc67c84912929f3b16ed0487aa2d3
dimatosdev <dimatosdev@gmail.com>
2025-06-10 01:40:58 -0300

    Add DICOM image upload controller, model, and migration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DicomImageController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// API Routes for DICOM Images
+Route::prefix('api')->group(function () {
+    Route::resource('dicom-images', DicomImageController::class);
+    Route::get('dicom-images/{dicomImage}/download', [DicomImageController::class, 'download'])->name('dicom-images.download');
 });
